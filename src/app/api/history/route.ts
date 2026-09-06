c920fdca7d51378f1acf0f3845172824cb382b78
Karim Shehadeh <kshehadeh@underarmour.com>
2026-09-06 05:18:04 -0400

    Release v0.1.0 appcast

--- src/app/api/history/route.ts
+++
@@ -1,8 +0,0 @@
-import { attemptsRepo } from "@/server/db/repositories";
-import { withGuard } from "@/server/http";
-
-export const runtime = "nodejs";
-
-export const GET = withGuard(async () => {
-  return Response.json({ attempts: attemptsRepo().list() });
-});
--- src/app/compose/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Composer } from "@/components/Composer";
-
-export default function Page() {
-  return <Composer />;
-}
